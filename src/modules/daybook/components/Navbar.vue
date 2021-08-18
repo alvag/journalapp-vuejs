84bda679f6debd8a7743bb904e8e79907c0ce2a4
Max Alva <alva85@gmail.com>
2021-08-17 23:23:57 -0500

    Estructura del DaybookLayout

---
+++ src/modules/daybook/components/Navbar.vue
@@ -0,0 +1,26 @@
+<template>
+    <nav class="navbar bg-primary">
+        <a class="navbar-brand text-white">
+            <img class="d-inline-block align-text-top mx-2"
+                 height="24"
+                 src="@/assets/logo.png" alt="logo">
+            Daybook
+        </a>
+
+        <div class="d-flex">
+            <button class="btn btn btn-outline-info mx-2">
+                <i class="fa fa-sign-out-alt"></i>
+            </button>
+        </div>
+    </nav>
+</template>
+
+<script>
+export default {
+    name: 'Navbar'
+};
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/modules/daybook/layouts/DaybookLayout.vue
@@ -0,0 +1,27 @@
+<template>
+    <Navbar/>
+
+    <div class="d-flex ">
+        <div class="col-4">
+            <EntryList/>
+        </div>
+
+        <div class="col">
+            <router-view/>
+        </div>
+    </div>
+</template>
+
+<script>
+import Navbar from '@/modules/daybook/components/Navbar';
+import EntryList from '@/modules/daybook/components/EntryList';
+
+export default {
+    name: 'DaybookLayout',
+    components: { EntryList, Navbar }
+};
+</script>
+
+<style scoped>
+
+</style>
