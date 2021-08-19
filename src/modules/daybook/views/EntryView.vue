46d4df7ec2241db7de359f659ead01b934248e25
Max Alva <alva85@gmail.com>
2021-08-19 17:45:45 -0500

    EntryView

--- src/modules/daybook/views/EntryView.vue
+++ src/modules/daybook/views/EntryView.vue
@@ -1,36 +1,40 @@
 <template>
-    <div class="entry-title d-flex justify-content-between p-2">
-        <div>
-            <span class="text-success fs-3 fw-bold">{{ date.day }}</span>
-            <span class="fs-3 mx-1">{{ date.month }}</span>
-            <span class="fs-4 mx-2 fw-light">{{ date.yearDay }}</span>
+    <template v-if="entry">
+        <div class="entry-title d-flex justify-content-between p-2">
+            <div>
+                <span class="text-success fs-3 fw-bold">{{ date.day }}</span>
+                <span class="fs-3 mx-1">{{ date.month }}</span>
+                <span class="fs-4 mx-2 fw-light">{{ date.yearDay }}</span>
+            </div>
+
+            <div>
+                <button class="btn btn-danger mx-2">
+                    Borrar
+                    <i class="fa fa-trash-alt"></i>
+                </button>
+
+                <button class="btn btn-primary mx-2">
+                    Subir foto
+                    <i class="fa fa-upload"></i>
+                </button>
+            </div>
         </div>
 
-        <div>
-            <button class="btn btn-danger mx-2">
-                Borrar
-                <i class="fa fa-trash-alt"></i>
-            </button>
+        <hr>
 
-            <button class="btn btn-primary mx-2">
-                Subir foto
-                <i class="fa fa-upload"></i>
-            </button>
-        </div>
-    </div>
-
-    <hr>
-
-    <div class="d-flex flex-column px-3 h-75">
+        <div class="d-flex flex-column px-3 h-75">
         <textarea v-model="entry.text"
                   class="form-control shadow-none"
-                  placeholder="¿Qué sucedió hoy?"></textarea>
-    </div>
+                  placeholder="¿Qué sucedió hoy?">
+        </textarea>
+        </div>
 
-    <img alt="entry picture"
-         class="img-thumbnail"
-         src="https://fotoarte.com.uy/wp-content/uploads/2019/03/Landscape-fotoarte.jpg">
+        <img alt="entry picture"
+             class="img-thumbnail"
+             src="https://fotoarte.com.uy/wp-content/uploads/2019/03/Landscape-fotoarte.jpg">
 
+    </template>
+    
     <Fab icon="fa-save"/>
 </template>
 
@@ -47,13 +51,18 @@ export default {
             required: true
         }
     },
-    daa() {
+    data() {
         return {
             entry: null
         };
     },
     created() {
         this.loadEntry();
+    },
+    watch: {
+        id() {
+            this.loadEntry();
+        }
     },
     computed: {
         ...mapGetters( 'journal', ['getEntryById'] ),
