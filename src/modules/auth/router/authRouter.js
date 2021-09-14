1d224beb2bed77c2c0738028a3c45be2b1a5e9bf
Max Alva <alva85@gmail.com>
2021-09-14 13:04:32 -0500

    auth router

--- src/modules/auth/router/authRouter.js
+++ src/modules/auth/router/authRouter.js
@@ -1,5 +1,16 @@
 export default {
 	name: 'auth',
 	component: () => import(/* webpackChunkName: "AuthLayout" */ '@/modules/auth/layouts/AuthLayout'),
-	children: []
+	children: [
+		{
+			path: '',
+			name: 'login',
+			component: () => import(/* webpackChunkName: "Login" */ '@/modules/auth/views/Login'),
+		},
+		{
+			path: 'register',
+			name: 'register',
+			component: () => import(/* webpackChunkName: "Login" */ '@/modules/auth/views/Register'),
+		}
+	]
 };
